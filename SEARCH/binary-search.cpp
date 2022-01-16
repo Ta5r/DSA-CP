3d7c2c8126cb389e309133554fd19b60580785c6
Ta5r <srv.tanay@gmail.com>
2022-01-16 13:17:40 +0530

    SEARCH : LINEAR and BINARY

---
+++ SEARCH/binary-search.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+//using RECURSIVE FUNCTION CALLS
+
+// A recursive binary search function. It returns
+// location of x in given array arr[l..r] is present,
+// otherwise -1
+int binarySearch(int arr[], int l, int r, int x)
+{
+	if (r >= l) {
+		int mid = l + (r - l) / 2;
+
+		// If the element is present at the middle
+		// itself
+		if (arr[mid] == x)
+			return mid;
+
+		// If element is smaller than mid, then
+		// it can only be present in left subarray
+		if (arr[mid] > x)
+			return binarySearch(arr, l, mid - 1, x);
+
+		// Else the element can only be present
+		// in right subarray
+		return binarySearch(arr, mid + 1, r, x);
+	}
+
+	// We reach here when element is not
+	// present in array
+	return -1;
+}
+
+//using ITERATIONS
+
+int binarySearch2(int arr[], int n ,int x)
+{
+    int low = 0 , high = n-1;
+    
+    while(low < high)
+    {
+        int mid = (low+high)/2;
+
+        if(arr[mid]>x)
+            high = mid-1;    
+        else if(arr[mid]<x)    
+            low = mid+1;    
+        else    
+            return mid;    
+    }
+    return -1;
+}
+
+int main(void)
+{
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int x = 10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int result = binarySearch2(arr,n, x);
+	(result == -1) ? cout << "Element is not present in array"
+				: cout << "Element is present at index " << result;
+	return 0;
+}
